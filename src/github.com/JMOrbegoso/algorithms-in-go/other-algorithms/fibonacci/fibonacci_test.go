6280ed1b1227d79eef0f42e48ea47f3e9d6973a4
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-01-20 11:21:26 -0500

    Create fibonacci_test.go

---
+++ src/github.com/JMOrbegoso/algorithms-in-go/other-algorithms/fibonacci/fibonacci_test.go
@@ -0,0 +1,31 @@
+package fibonacci
+
+import "testing"
+
+type testType struct {
+	input uint8
+	want  uint64
+}
+
+func TestFibonacci(t *testing.T) {
+	// Arrange
+	testsValues := []testType{
+		{0, 0},
+		{1, 1},
+		{2, 1},
+		{10, 55},
+		{15, 610},
+		{30, 832040},
+		{40, 102334155},
+	}
+
+	// Act
+	for i := 0; i < len(testsValues); i++ {
+		output := fibonacci(testsValues[i].input)
+
+		// Assert
+		if output != testsValues[i].want {
+			t.Fatalf("when the input is %v, it should return %v, but got %v", testsValues[i].input, testsValues[i].want, output)
+		}
+	}
+}
