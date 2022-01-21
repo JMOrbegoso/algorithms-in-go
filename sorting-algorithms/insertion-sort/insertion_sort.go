77e5e69b9a81ed6745a6f0f97c5cf8e933c68b7e
JM Orbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-01-21 17:02:00 -0500

    Update insertion_sort.go to use the keyword 'range'

--- sorting-algorithms/insertion-sort/insertion_sort.go
+++ sorting-algorithms/insertion-sort/insertion_sort.go
@@ -3,7 +3,7 @@ package insertion_sort
 func insertionSort(array []int16) []int16 {
 	var key int16
 
-	for i := 1; i < len(array); i++ {
+	for i := range array {
 		key = array[i]
 
 		for j := i - 1; 0 <= j; j-- {
